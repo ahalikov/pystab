7810cf08a2fa5e23c774bb7d9f903e834700d545
dina.kayumova <dina.kayumova@62592d5c-0a48-11df-9a7a-53bbb444ad82>
2010-04-07 17:54:53 +0000

    добавлены два модуля. deformable.py - для исследования систем с произвольным числом деформируемых колес. Пока реализована только одна теория - теория Келдыша, описывающая взаимодействие колеса с дорогой. const.py - отвечает за именованные константы

---
+++ src/pystab/const.py
@@ -0,0 +1,13 @@
+# this recipe was taken from http://www.onlamp.com/pub/a/python/2002/07/11/recipes.html
+class _Constants:
+  class ConstError(TypeError): pass
+  def __setattr__(self, name, value):
+    if self.__dict__.has_key(name):
+      raise self.ConstError, "Can't rebind const(%s)"%name
+    self.__dict__[name ] = value
+  def __delattr__(self, name):
+    if self.__dict__.has_key(name):
+      raise self.ConstError, "Can't unbind	const(%s)"%name
+    raise NameError, name
+import sys
+sys.modules[__name__] = _Constants()
